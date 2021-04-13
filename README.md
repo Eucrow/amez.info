e910ed0723584cf4663ec90cd3a69e4402494fcb
Marco A. Amez <marco.a.amez@gmail.com>
2021-04-13 19:32:40 +0200

    Minor changes.

--- README.md
+++ README.md
@@ -4,4 +4,4 @@
 
 This is the work in progress of my basic personal web. You can see the distribution version in [this](https://github.com/Eucrow/amez.info.dist) repository. An you can see it in http://www.amez.info.
 
-As you can see, contains the package.json file of npm and the gulpfile.js of gulp. I use sass, so the sass files are also included in order to show to the world my increasing continuous knowledge of web develop :-).
+As you can see, contains the package.json file of npm and the gulpfile.js of gulp. I use sass, so the sass files are also included in order to show to the world my increasing continuous knowledge of web develop :P.
--- gulpfile.js
+++ gulpfile.js
@@ -1,73 +1,88 @@
-'use strict';
+"use strict";
 
-var gulp = require('gulp');
-var sass = require('gulp-sass');
-var browserSync = require('browser-sync').create();
+var gulp = require("gulp");
+var sass = require("gulp-sass");
+var browserSync = require("browser-sync").create();
 
-sass.compiler = require('node-sass');
+sass.compiler = require("node-sass");
 
-var responsive = require('gulp-responsive-images');
+var responsive = require("gulp-responsive-images");
 
-var webp = require('gulp-webp');
- 
+var webp = require("gulp-webp");
+
+const cleanCSS = require("gulp-clean-css");
+
+// minify css
+gulp.task("minify-css", () => {
+  return gulp
+    .src("../amez.info.dist/css/*.css")
+    .pipe(cleanCSS({ compatibility: "ie8" }))
+    .pipe(gulp.dest("../amez.info.dist/css"));
+});
+
 // copy required files to dist folder
-function copyToDistFolder(){
-  return gulp.src('./index.html')
-      .pipe(gulp.dest('../amez.info.dist'))
-      .pipe(browserSync.stream());
+function copyToDistFolder() {
+  return gulp
+    .src("./index.html")
+    .pipe(gulp.dest("../amez.info.dist"))
+    .pipe(browserSync.stream());
 }
 
 // copy required files to imgs folder
-function copyToImgsFolder(){
-  return gulp.src('./imgs/*.{svg,jpg,webp}')
-      .pipe(gulp.dest('../amez.info.dist/imgs'))
-      .pipe(browserSync.stream());
+function copyToImgsFolder() {
+  return gulp
+    .src("./imgs/*.{svg,jpg,webp}")
+    .pipe(gulp.dest("../amez.info.dist/imgs"))
+    .pipe(browserSync.stream());
 }
 
 // sass compiler
-function compileSass(){
-  return gulp.src('./scss/**/*.scss')
-    .pipe(sass().on('error', sass.logError))
-    .pipe(gulp.dest('../amez.info.dist/css'))
+function compileSass() {
+  return gulp
+    .src("./scss/**/*.scss")
+    .pipe(sass().on("error", sass.logError))
+    .pipe(gulp.dest("../amez.info.dist/css"))
     .pipe(browserSync.stream());
 }
 
 // watch files
-function watchFiles(){
-  gulp.watch('./scss/*.scss', compileSass);
-  gulp.watch("./*.html").on('change', copyToDistFolder);
-  gulp.watch("./imgs/*.{svg,jpg,webp}").on('change', copyToImgsFolder);
+function watchFiles() {
+  gulp.watch("./scss/*.scss", compileSass);
+  gulp.watch("./*.html").on("change", copyToDistFolder);
+  gulp.watch("./imgs/*.{svg,jpg,webp}").on("change", copyToImgsFolder);
 }
 
 // Run static server and watch files
-gulp.task("default", gulp.parallel(
-  copyToImgsFolder,
-  watchFiles, 
-  function() {
+gulp.task(
+  "default",
+  gulp.parallel(copyToImgsFolder, watchFiles, function () {
     browserSync.init({
-        server: "../amez.info.dist",
-        //server: "./",
-        port: 8000,
-        browser: 'chrome'
+      server: "../amez.info.dist",
+      //server: "./",
+      port: 8000,
+      browser: "chrome",
     });
-  }
-));
+  })
+);
 
 /**
  * Export images to responsive resolutions and export to webp format.
  * Take images from img/originals directory.
  * Use only when images has been changed.
  */
-gulp.task('responsiveImages', function(){
-  return gulp.src('original_images/*.jpg') // WARNING: can't be spaces between 'jpg,' and 'png'
-      .pipe(responsive({ // create responsive images
-          '*.jpg':[
-              { width: 400, quality: 90, suffix: "_400" },
-              { width: 768, quality: 90, suffix: "_768" },
-              { width: 1280, quality: 85, suffix: "_1280" }
-          ]
-      }))
-      .pipe(webp()) // export to webp format
-      .pipe(gulp.dest('imgs/'))
-})
-
+gulp.task("responsiveImages", function () {
+  return gulp
+    .src("original_images/*.jpg") // WARNING: can't be spaces between 'jpg,' and 'png'
+    .pipe(
+      responsive({
+        // create responsive images
+        "*.jpg": [
+          { width: 400, quality: 90, suffix: "_400" },
+          { width: 768, quality: 90, suffix: "_768" },
+          { width: 1280, quality: 85, suffix: "_1280" },
+        ],
+      })
+    )
+    .pipe(webp()) // export to webp format
+    .pipe(gulp.dest("imgs/"));
+});
